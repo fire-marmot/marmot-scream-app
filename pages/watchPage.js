002ec12a8bb7f7a743dec51d4cdb0ebc94b12e51
Clarissa P <clarissapamonicutt@gmail.com>
2022-05-14 13:56:48 -0700

    watchpage created

---
+++ pages/watchPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../sharedComponents/NavBar.js';
+import Header from './sharedComponents/Header.js'
+
+const watchPage = () => {
+
+  return (
+    <>
+    <NavBar/>
+    <Header/>
+    </>
+  )
+}
+
+export default watchPage;
