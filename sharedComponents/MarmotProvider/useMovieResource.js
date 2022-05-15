682c5a44d8f409fbc29ee80b3c8f33602fd6dc33
Chris Rarig <cwrarig20@gmail.com>
2022-05-14 21:40:26 -0500

    updated post route

--- sharedComponents/MarmotProvider/useMovieResource.js
+++ sharedComponents/MarmotProvider/useMovieResource.js
@@ -1,21 +1,21 @@
 import axios from 'axios';
 import useSWR from 'swr';
 
-export const apiUrl = process.env.NEXT_PUBLIC_RESOURCE_URL;
-export const user = process.env.NEXT_PUBLIC_USERNAME;
-export const pass = process.env.NEXT_PUBLIC_PASSWORD;
+export const apiUrl = 'http://localhost:8000/api/token/';
+export const user = 'admin1';
+export const pass = 'admin1';
 
 
 export const useMovieResource = () => {
   
   const fetchResource = async () => {
     try {
-      const response = await axios.get(apiUrl, {},{
-        auth:{
+      const response = await axios.post(apiUrl, {
           username: 'admin1',
-          password: 'admin1',
+          password: 'admin1'
         }
-      });
+      );
+  
       console.log(response.data);
       return response.data;
     } catch (err) {
