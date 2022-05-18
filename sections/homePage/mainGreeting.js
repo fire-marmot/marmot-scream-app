ed4cd9cc7fcc0e73cb5e5c006d07c02261de9351
Clarissa P <clarissapamonicutt@gmail.com>
2022-05-17 20:49:57 -0700

    css stuff

--- sections/homePage/mainGreeting.js
+++ sections/homePage/mainGreeting.js
@@ -3,14 +3,18 @@ import Image from 'next/image';
 
 const MainGreeting = () => (
   <>
-    <Image
-      src='/public/marmothipster.jpg'
-      alt='marmothipster'
-      width={100}
-      height={100}
-    />
+    <div className='flex justify-center m-5'>
+      <Image
+        src='/marmothipster.jpg'
+        alt='marmothipster'
+        width={200}
+        height={200}
+        className='rounded-full'
+      />
 
-    <h1 className="p-3 font-bold text-center text-7xl text-primary"> Marmot Movies</h1>
+      <h1 className="p-3 ml-10 font-bold text-center text-7xl text-primary"> Marmot Movies</h1>
+      
+    </div>
   </>
 )
 
